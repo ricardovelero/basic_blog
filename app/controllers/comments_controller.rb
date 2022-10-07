3188124a86576dc878605827c77c3aa069eb125d
Ricardo Rodriguez <ricardovelero@gmail.com>
2022-10-07 23:15:16 +0200

    Install Devise

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-    http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+    before_action :authenticate_user!, except: [:index, :show]
     
     def create
       @article = Article.find(params[:article_id])
